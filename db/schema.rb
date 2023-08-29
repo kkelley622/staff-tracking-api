55e686080c3c1705624584bf23254677e3732d1e
Kevin Kelley <kkelley622@gmail.com>
2023-08-29 17:03:32 -0600

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_225850) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_230307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_225850) do
   create_table "staffs", force: :cascade do |t|
     t.text "name"
     t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
