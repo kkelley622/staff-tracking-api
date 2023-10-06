d2a6edb5cced3b25558068cdf7f2ddd4fb85c5a9
Kevin Kelley <kkelley622@gmail.com>
2023-10-05 21:26:49 -0500

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+kevin = User.create(username: "kevin.kelley", password: "Mprove354.", name: "Kevin Kelley");
+
+lisa_h = Staff.create(name: 'Lisa Hullinger', group_id: "2");
+
+group_1 = Group.create(number: 1);
+group_2 = Group.create(number: 2);
+group_3 = Group.create(number: 3);
+group_4 = Group.create(number: 4);
+
+seh = Account.create(name: "Southeast Health", group_id: 2);
+kd = Account.create(name: "Kings Daughter", group_id: 1);
