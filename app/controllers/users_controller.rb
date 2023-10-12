c7cbe3791f4c6897cc2b3de02ed3a7adb7e28f6a
Kevin Kelley <kkelley622@gmail.com>
2023-10-11 21:16:19 -0500

    Added user_params function, added create function to Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
         render json: users, status: :ok 
     end
 
+    # def me 
+    #     me = @user 
+    #     render json: 
+    # end
+    def create 
+        user = User.create!(user_params)
+        render json: user, status: :ok
+    end
+
     private
 
     def find_user
         @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :password, :name)
     end
 end
