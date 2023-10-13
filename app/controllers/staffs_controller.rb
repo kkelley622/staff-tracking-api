0fb783dfab7d4c91fe5f15b40515c296aa9362c4
Kevin Kelley <kkelley622@gmail.com>
2023-10-12 21:19:35 -0500

    Added create function to staffs controller.

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -3,4 +3,15 @@ class StaffsController < ApplicationController
         staffs = Staff.all
         render json: staffs, status: :ok;
     end
+
+    def create 
+        staff = Staff.create!(staff_params)
+        render json: staff, status: :ok
+    end
+end
+
+private
+
+def staff_params
+    params.permit(:name, :group_id)
 end
