43fe2cd2e206bff027f65b6ff152caf989c11a50
Kevin Kelley <kkelley622@gmail.com>
2023-10-12 21:22:41 -0500

    Added find_staff private function and destroy method to Staffs controller.

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -8,9 +8,18 @@ class StaffsController < ApplicationController
         staff = Staff.create!(staff_params)
         render json: staff, status: :ok
     end
+
+    def destroy 
+        @staff.destroy 
+        render json: @staff, status: :ok 
+    end
 end
 
 private
+
+def find_staff 
+    @staff = Staff.find(params[:id])
+end
 
 def staff_params
     params.permit(:name, :group_id)
