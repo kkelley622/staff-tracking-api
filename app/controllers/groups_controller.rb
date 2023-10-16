78d84f9e5599d5e714c7bfba6924483d340fbfed
Kevin Kelley <kkelley622@gmail.com>
2023-10-15 19:56:59 -0500

    Added private methods and index method to groups controller.'

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,5 +3,15 @@ class GroupsController < ApplicationController
     def index 
         groups = Group.all 
         render json: groups, status: :ok
-    end;
+    end
+
+    def show
+        render json: @group, status: :ok 
+    end
+
+    private
+
+    def find_group
+        @group = Group.find(params[:id])
+    end
 end
