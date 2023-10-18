560ddf03bb7028af432ecb11e290e4c361a82fdf
Kevin Kelley <kkelley622@gmail.com>
2023-10-17 20:22:17 -0500

    Added private methods and show and create methods to accounts controller.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,23 @@ class AccountsController < ApplicationController
         accounts = Account.all
         render json: accounts, status: :ok;
     end
+
+    def show 
+        render json: @account, status: :ok;
+    end
+
+    def create 
+        new_group = Group.create!(group_params)
+        render json: new_group, status: :ok;
+    end
+
+    private
+
+    def find_account 
+        @account = Account.find(params[:id])
+    end
+
+    def group_params
+        params.permit(:name, :group_id)
+    end
 end
