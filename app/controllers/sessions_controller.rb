70f55e930bf57af3d77cb7f4fb528ffd20038cd7
Kevin Kelley <kkelley622@gmail.com>
2023-10-18 21:07:22 -0500

    Added sessions controller and create and destroy methods.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    before_action :logged_in, only: [:create]
+    skip_before_action :authorize, only: [:create]
+    
+    def create 
+        user = User.find_by_user_name(params[:user_name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: { errors: ["Username or Password Didn't Match"]}, status: :unprocessable_entity
+        end 
+    end 
+
+    def destroy 
+        session.delete :user_id
+    end
+
+end
