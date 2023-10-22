0e599fe45ede79a6b599e28ac83eedb737c53a0d
Kevin Kelley <kkelley622@gmail.com>
2023-10-22 17:48:19 -0500

    Added bcrypt gem.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :groups, only [:index]
+  resources :users;
+  resources :staffs;
 end
